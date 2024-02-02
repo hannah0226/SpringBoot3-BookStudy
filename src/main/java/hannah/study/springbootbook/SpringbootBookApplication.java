04707439475616aac840d97348b5bafe3b6c37b5
hannah0226 <mohan0226@naver.com>
2024-02-02 18:32:16 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/hannah/study/springbootbook/SpringbootBookApplication.java
@@ -0,0 +1,13 @@
+package hannah.study.springbootbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBookApplication.class, args);
+	}
+
+}
